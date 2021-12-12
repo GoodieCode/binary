984eb8b06653ef438d1cb12ef446e166a8e7b223
GoodieCode <73887057+GoodieCode@users.noreply.github.com>
2021-12-12 18:31:00 +0000

    added conversion from hex to decimal

--- hex.py
+++ hex.py
@@ -1,4 +1,5 @@
-def toHex(number:int):
+#Converts a decimal number to binary
+def toHex(number:int) -> list:
     hex_array:chr = [] #Character array to store the hexidecimal numbers
 
     while number != 0:
@@ -14,8 +15,42 @@ def toHex(number:int):
     
     return hex_array
 
-def outputHex(hex_array:list) -> str:
+# Turns the hexidecimal list to a string output
+def buildHexString(hex_array:list) -> str:
+    output = ""
+    
     for i in range(len(hex_array), 0, -1):
-        print(hex_array[i - 1], end= '')
+        output += str(hex_array[i - 1])
+    
+    return output
 
-outputHex(toHex(33))
+
+# Converts a value from hexidecimal to Decimal
+def fromHex(digit) -> int:
+    digit_values = len(digit)
+    decimal = 0
+
+    base = 1
+
+    for i in range(digit_values - 1, -1, -1):
+        print("test")
+        # Numbers lie between 0 and 9
+        if digit[i] >= '0' and digit[i] <= '9':
+            decimal += (ord(digit[i]) - 48) * base
+
+            base = base * 16
+
+        # Numbers are between A(10) and F(16)
+        elif digit[i] >= 'A' and digit[i] <= 'F':
+            decimal += (ord(digit[i]) - 55) * base 
+
+            base = base * 16
+
+    return decimal
+
+hex_values = toHex(1019)
+str_hex = buildHexString
+(hex_values)
+dec_value = fromHex(str_hex)
+
+print(f"Hex_list = {hex_values}, string_hex = {str_hex}, Decimal =  {dec_value}")
